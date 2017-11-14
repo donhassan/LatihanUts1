e90838fbbf7a9d8106021d1ac0b7557e53c4085b
USER <USER@DESKTOP-68B98IN>
2017-11-14 22:18:29 +0700

    Latihan Uts 1

---
+++ src/model/modelMahasiswa.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author USER
+ */
+public class modelMahasiswa {
+    public String nim,nama;
+    public modelMahasiswa (String nim, String nama){
+        this.nim=nim;
+        this.nama=nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+    
+    
+    
+ 
+    
+}
